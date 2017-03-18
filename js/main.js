d7af70401e5448ccb666ec00b3a9064d98dfb7ee
Joanna Colson <Joanna Colson>
2017-03-18 03:55:50 -0700

    updated game logic to include functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-//console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-//This is the first card that the user flipped.
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-//This is the second card that the user flipped.
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-//Check to see if user played two cards.
-if (cardsInPlay.length === 2) {
+
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	//Check to see if user played two cards.
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
