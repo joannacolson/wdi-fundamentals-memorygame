2df8fa008b79362d936549ae5d7af32e1a11496b
Joanna Colson <Joanna Colson>
2017-03-18 00:50:56 -0700

    created initial game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 //console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+//This is the first card that the user flipped.
+var cardOne = cards [0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//This is the second card that the user flipped.
+var cardTwo = cards [2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+//Check to see if user played two cards.
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
